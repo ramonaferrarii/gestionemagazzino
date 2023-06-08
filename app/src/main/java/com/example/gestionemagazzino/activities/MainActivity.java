f4e74aa827f160701abc69f4a1ba5ddc4f4cfbd3
Ramona <ramonaferrair01@hotmail.it>
2023-06-08 16:12:59 +0200

    implemented listener
    half kick

--- app/src/main/java/com/example/gestionemagazzino/activities/MainActivity.java
+++ app/src/main/java/com/example/gestionemagazzino/activities/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.gestionemagazzino.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.gestionemagazzino.R;
 
@@ -12,5 +15,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+
+
+
+
+
+
+
+        }
+
+    ButtonListener buttonListener = new ButtonListener();
 }
+
+
+
+    class ButtonListener implements OnClickListener {
+    // definisco listener
+    private View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.B_mainspace:
+
+
+                    break;
+
+                case R.id.B_aspiraz:
+
+
+                    break;
+
+                case R.id.B_dae:
+
+                    break;
+
+                case R.id.B_backpack:
+
+                    break;
+
+                case R.id.B_childpack:
+
+                    break;
+
+                case R.id.B_misc:
+
+                    break;
+
+                case R.id.B_dpi:
+
+                    break;
+
+                case R.id.B_trauma:
+
+                    break;
+
+                case R.id.B_frontspace:
+
+                    break;
+
+
+            }
+        }
+    };
+}
+
+
