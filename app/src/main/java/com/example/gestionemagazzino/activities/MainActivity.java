30a4a52113b9ac452438b671bd6b06d45488f4b6
Ramona <ramonaferrair01@hotmail.it>
2023-06-12 12:07:46 +0200

    added the arrow go back in main activity

--- app/src/main/java/com/example/gestionemagazzino/activities/MainActivity.java
+++ app/src/main/java/com/example/gestionemagazzino/activities/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.gestionemagazzino.activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,7 +18,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+         // bottone per tornare indietro
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
 
     ButtonListener buttonListener = new ButtonListener();
 }
