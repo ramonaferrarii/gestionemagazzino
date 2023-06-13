efe81ecb343220c27a6177c1d6fdbf976898d71f
Ramona <ramonaferrair01@hotmail.it>
2023-06-13 14:11:16 +0200

    modified with FragmentManager

--- app/src/main/java/com/example/gestionemagazzino/activities/MainActivity.java
+++ app/src/main/java/com/example/gestionemagazzino/activities/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.gestionemagazzino.activities;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.app.Fragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.gestionemagazzino.R;
+import com.example.gestionemagazzino.fragments.ButtonsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,13 +22,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
          // bottone per tornare indietro
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-        }
+
+
+        // devo definire la chiamata al ButtonsFragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.fragment_container_view, ButtonsFragment.class, null)
+                .setReorderingAllowed(true)
+                .addToBackStack("name") // Name can be null
+                .commit();
+
+
+    }
 
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()) {
@@ -47,9 +60,9 @@ public class MainActivity extends AppCompatActivity {
         // definisco listener
         // private View.OnClickListener buttonListener = new View.OnClickListener() {
 
-            @Override
+           @Override
             public void onClick(View view) {
-                switch (view.getId()) {
+              /*  switch (view.getId()) {
                     case R.id.B_mainspace:
 
 
@@ -89,8 +102,15 @@ public class MainActivity extends AppCompatActivity {
                         break;
 
                 }
+
+          */
             };
+
+
         };
+
+
+
 
 
 
