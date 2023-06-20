5fb3035eac5b681acc0448cc9fe52e4158f7433c
Ramona <ramonaferrair01@hotmail.it>
2023-06-20 12:27:57 +0200

    added botton Save in some bottons
    ne mancano 5

--- app/src/main/java/com/example/gestionemagazzino/fragments/DaeFragment.java
+++ app/src/main/java/com/example/gestionemagazzino/fragments/DaeFragment.java
@@ -4,10 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.gestionemagazzino.R;
+import com.example.gestionemagazzino.models.FirebaseWrapper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DaeFragment extends Fragment {
 
@@ -19,6 +27,15 @@ public class DaeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private String[] dbKeys={"dae","Garze Non Sterili","Piastre Monouso", "Rasoio"};
+
+    private ArrayList<EditText> editTextsList = new ArrayList<EditText>();
+
+    private EditText dae;
+    private EditText garzenonsterili;
+    private EditText piastremonuso;
+    private EditText rasoio;
 
     public DaeFragment() {
         // Required empty public constructor
@@ -55,6 +72,32 @@ public class DaeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dae, container, false);
+        //return inflater.inflate(R.layout.fragment_dae, container, false);
+        View externalView = inflater.inflate(R.layout.fragment_dae, container, false);
+        editTextsList.add(externalView.findViewById(R.id.ET_dae));
+        editTextsList.add(externalView.findViewById(R.id.ET_garze_non_sterili));
+        editTextsList.add(externalView.findViewById(R.id.ET_piastre_monouso));
+        editTextsList.add(externalView.findViewById(R.id.ET_rasoio));
+        Button saveButton = externalView.findViewById(R.id.B_save);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HashMap<String, Integer> editTextValues= new HashMap<>();
+                for(int i=0;i<4;i++)
+                    editTextValues.put(dbKeys[i], Integer.parseInt(editTextsList.get(i).getText().toString()));
+
+
+                FirebaseWrapper.RTDatabase RTdb = new FirebaseWrapper.RTDatabase();
+                for (Map.Entry<String, Integer> entry : editTextValues.entrySet()){
+                    RTdb.updateDbData("Aspirazione",entry.getKey(),entry.getValue());
+                }
+                for (EditText editText : editTextsList)
+                    editText.setText("0");
+                CharSequence msg="parametri salvati";
+                Toast.makeText(externalView.getContext(), msg, Toast.LENGTH_SHORT).show();
+                //TODO: handle the case where the user submits an empty editText
+            }
+        });
+        return externalView;
     }
 }
