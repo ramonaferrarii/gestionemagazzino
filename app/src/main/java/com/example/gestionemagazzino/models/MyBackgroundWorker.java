4b490a6e6d70cb4bb03b699e4558b8cb91d9b020
Enrico Dondero <enrico.dondero.p@gmail.com>
2023-06-21 11:46:09 +0200

    background worker fix

--- app/src/main/java/com/example/gestionemagazzino/models/MyBackgroundWorker.java
+++ app/src/main/java/com/example/gestionemagazzino/models/MyBackgroundWorker.java
@@ -20,6 +20,8 @@ import java.util.Random;
 
 public class MyBackgroundWorker extends Worker{
 
+    private static final String[] docs = {"Aspirazione","DPI", "Defibrillatore", "KitAmbu", "Trauma", "Ustione", "Vano Guida", "Varie", "ZainoPediatrico", "medicazione", "parto"};
+
     private static final int PERMISSION_REQUEST_CODE=(new Random()).nextInt() & Integer.MAX_VALUE;
     public MyBackgroundWorker(@NonNull Context context,@NonNull WorkerParameters workerParams){
         super(context,workerParams);
@@ -28,27 +30,29 @@ public class MyBackgroundWorker extends Worker{
     @NonNull
     @Override
     public Result doWork(){
-        //TODO: extend to every field, now just for testing purposes we are using only the "Aspirazione" document
-        FirebaseWrapper.RTDatabase db = new FirebaseWrapper.RTDatabase();
-        db.readDbData("Aspirazione", new FirebaseWrapper.RTDatabase.FirestoreCallback() {
-            @Override
-            public void onCallback(HashMap<String, Object> data) {
-                ArrayList<String> objs = new ArrayList<>();
-                if (data != null) {
-                    //check every entry in the desired document for quantities inferior to 5
-                    for (Map.Entry<String, Object> entry : data.entrySet()) {
-                        if ((Long)entry.getValue()<5){
-                            objs.add(entry.getKey());
 
+        FirebaseWrapper.RTDatabase db = new FirebaseWrapper.RTDatabase();
+        for(int i=0; i<docs.length; i++){
+            db.readDbData(docs[i], new FirebaseWrapper.RTDatabase.FirestoreCallback() {
+                @Override
+                public void onCallback(HashMap<String, Object> data) {
+                    ArrayList<String> objs = new ArrayList<>();
+                    if (data != null) {
+                        //check every entry in the desired document for quantities inferior to 5
+                        for (Map.Entry<String, Object> entry : data.entrySet()) {
+                            if ((Long)entry.getValue()<5){
+                                objs.add(entry.getKey());
+
+                            }
                         }
+                        String obj= String.join(", ",objs);
+                        //notification function
+                        SendNotification("oggetto in esaurimento: "+obj);
                     }
-                    String obj= String.join(", ",objs);
-                    //notification function
-                    SendNotification("oggetto in esaurimento: "+obj);
                 }
-            }
-        });
-        //TODO: to be redefined
+            });
+            //TODO: to be redefined
+        }
         return Result.success();
     }
 
@@ -58,7 +62,8 @@ public class MyBackgroundWorker extends Worker{
                 .setContentTitle("Materiale in esaurimento")
                 .setContentText(msg)
                 .setSmallIcon(R.drawable.ic_stat_onesignal_default)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg));
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
         /*MainActivity main = (MainActivity)getApplicationContext();
